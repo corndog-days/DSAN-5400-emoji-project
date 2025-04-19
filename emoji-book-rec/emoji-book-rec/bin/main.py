e8aac5bbc75b52ccb6fbdfa3a4e32111582965a5
Lillian Ehrhart <lillian.m.ehrhart@gmail.com>
2025-04-19 14:25:19 -0400

    adding index.py correctly

---
+++ emoji-book-rec/emoji-book-rec/bin/main.py
@@ -0,0 +1,5 @@
+from emoji_book_rec.emoji_book_rec.utils.index import create_index
+from emoji_book_rec.emoji_book_rec.utils.query import process_query
+
+# Create keyword-book index
+kw_book_index = create_index(books)
