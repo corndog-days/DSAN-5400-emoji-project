172cfff9b89a5aa689cb3461af31bdca2c315763
isabelle <icr16@georgetown.edu>
2025-04-22 23:50:18 -0400

    Updates to GUI

--- emoji-book-rec/emoji-book-rec/utils/__init__.py
+++ emoji-book-rec/emoji-book-rec/utils/__init__.py
@@ -1,2 +1,4 @@
 from . import index
 from . import query
+from . import create_kw_book_tsv
+from . import main
--- emoji-book-rec/emoji-book-rec/utils/keyword_tsv_to_dict.py
+++ emoji-book-rec/emoji-book-rec/utils/keyword_tsv_to_dict.py
@@ -1,6 +1,19 @@
 #turns keyword tsv file from data folder into a dictionary and returns it
 
+#used ChatGPT to help write parts of this function
 def generate_keyword_dict(filepath):
    #given a filepath as a string, create a dictionary
    #shorttext as key, list of keywords as value
-    return
+   keyword_dict = {}
+
+   with open(filepath) as file:
+       # skipping the header
+       next(file)
+       for line in file:
+           parts = line.strip().split('\t')
+           if parts:  # make sure the line is not empty
+               shorttext = parts[0]
+               keywords = parts[1:]
+               keyword_dict[shorttext] = keywords
+   print(keyword_dict)
+   return
