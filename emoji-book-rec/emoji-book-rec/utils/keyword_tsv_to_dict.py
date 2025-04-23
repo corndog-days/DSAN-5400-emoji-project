99e293aafcafe21a0d44a3eafcc114367a5f1754
isabelle <icr16@georgetown.edu>
2025-04-23 12:12:15 -0400

    Updates to GUI

--- emoji-book-rec/emoji-book-rec/utils/keyword_tsv_to_dict.py
+++ emoji-book-rec/emoji-book-rec/utils/keyword_tsv_to_dict.py
@@ -3,7 +3,7 @@
 #used ChatGPT to help write parts of this function
 def generate_keyword_dict(filepath):
    #given a filepath as a string, create a dictionary
-   #shorttext as key, list of keywords as value
+   #returns dict with shorttext as key, list of keywords as value
    keyword_dict = {}
 
    with open(filepath) as file:
