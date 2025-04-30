eca5076a805bb8302b4bcef088d9e83f522d6cfa
corndog-days <122633823+corndog-days@users.noreply.github.com>
2025-04-30 18:06:39 -0400

    Including support for logging and weighting for different keywords

--- emoji_book_rec/emoji_book_rec/utils/query.py
+++ emoji_book_rec/emoji_book_rec/utils/query.py
@@ -4,31 +4,39 @@
 
 from collections import Counter
 import pandas as pd
+import logging
+from datetime import datetime
 
 from .keyword_tsv_to_dict import generate_keyword_dict
 from .index import create_index
 
-
 def process_query(query, filepath, use_precomputed=True, matrix_path=None):
     """
 
-        :param query: List of emoji queries from user in Unicode
+	:param query: List of emoji queries from user in Unicode
 
-        :param filepath: File path for emoji keyword list
+	:param filepath: File path for emoji keyword list
 
-        :param use_precomputed: If True, use a precomputed keyword-book matrix
+	:param use_precomputed: If True, use a precomputed keyword-book matrix
 
     :param matrix_path: Required if use_precomputed=True; path to the TSV matrix
 
-        :return: Sorted dictionary of book titles
+	:return: Sorted dictionary of book titles
 
-    """
+	"""
+
+    #start logging
+    output_file = 'emoji_book_rec/logs.txt'
+    logging.basicConfig(filename='logs.txt', level=logging.INFO)
+    logging.info(f'"{datetime.now()}: Query submitted. ************************************"')
+    logging.info(f'"Generating keyword dictionary from {filepath}"')
+    logging.info(f'"Writing to {output_file}"')
 
     # emoji_kw_dict: Dictionary of emojis and associated keywords
     emoji_kw_dict = generate_keyword_dict(filepath)
 
     # book index
-    # kw_book_index = create_index(books, emoji_kw_dict)  # build index
+    #kw_book_index = create_index(books, emoji_kw_dict)  # build index
 
     query_keywords = []
 
@@ -40,34 +48,52 @@ def process_query(query, filepath, use_precomputed=True, matrix_path=None):
     keyword_counts = Counter(query_keywords)
 
     # ----------------New logic----------------
-    print(query_keywords)
-    print(keyword_counts)
+    logging.info(f'"Query keywords: {query_keywords}"')
+    logging.info(f'"Keyword counts: {keyword_counts}"')
 
     if use_precomputed:
 
         if not matrix_path:
             raise ValueError("Matrix path required when use_precomputed=True")
 
-        matrix_df = pd.read_csv(matrix_path, sep="\t", index_col=0)
+        matrix_df = pd.read_csv(matrix_path, sep='\t', index_col=0)
 
         book_scores = {}
-
-        # for kw, multiplier in keyword_counts.items():
+        book_keyword_sets = {}
 
         for kw, count in keyword_counts.items():
-
             if kw in matrix_df.index:
-
                 for book_title, freq in matrix_df.loc[kw].items():
-                    # book_scores[book_title] = book_scores.get(book_title, 0) + freq * multiplier
-                    book_scores[book_title] = book_scores.get(book_title, 0) + freq * count
+                    if freq > 0:
+                        #count number of different keywords (got help from ChatGPT on how to design this part of the function)
+                        if book_title not in book_keyword_sets:
+                            book_keyword_sets[book_title] = set()
+                        if kw not in book_keyword_sets:
+                            book_keyword_sets[book_title].add(kw)
 
+                        book_scores[book_title] = book_scores.get(book_title, 0) + freq * count
+
+        book_different_keywords = {title: len(kw_set) for title, kw_set in book_keyword_sets.items()}
+
+        for title in book_scores:
+            book_scores[title] = book_scores[title] + (1.5*book_different_keywords[title])
+
+        logging.info(f'"Top 25 Search Results"')
+        #print top 25 books
         for i, (key, value) in enumerate(sorted(book_scores.items(), key=lambda x: x[1], reverse=True)):
             if i >= 25:
                 break
-            print(f"Key: {key}, Value: {value}")
+            logging.info(f'"Rank: {i+1}, Title: {key}, Score: {value}, Keywords found: {book_keyword_sets[key]}"')
 
+        #If uncommented: gives top 25 just on diversity of keywords
+        #for i, (key, value) in enumerate(sorted(book_different_keywords.items(), key=lambda x: x[1], reverse=True)):
+        #    if i >= 25:
+        #        break
+        #    print(f"Key: {key}, Value: {value}")
+
+        logging.info(f'"SEARCH COMPLETED ************************************"')
         return sorted(book_scores.items(), key=lambda x: x[1], reverse=True)
+
 
     # ----------------End of new logic----------------
 
@@ -86,3 +112,4 @@ def process_query(query, filepath, use_precomputed=True, matrix_path=None):
             else:
 
                 book_scores[book_title] = tf_count * count
+
